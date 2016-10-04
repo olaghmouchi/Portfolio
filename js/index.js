b596deebe89f05c52069b457d08ab678ff12e149
olaghmouchi <o.laghmouchi@gmail.com>
2016-10-04 09:21:46 +0200

    First commit

---
+++ js/index.js
@@ -0,0 +1,30 @@
+
+	document.getElementsByName('submit')[0].onclick = function()
+	{
+		if(checkContact())
+		{
+			document.querySelector(".warningMessage").innerHTML = "Gelukt";
+		} 
+		else 
+		{
+			document.querySelector(".warningMessage").innerHTML = "Controleer ingevoerde gegevens.";
+		}
+		
+	};
+
+	function checkContact()
+	{
+		var firstName = document.getElementsByName('firstname')[0].value;
+		var lastName = document.getElementsByName('lastname')[0].value;
+		var email = document.getElementsByName('email')[0].value;
+		
+		var namePattern = /^[A-z]+$/;
+		var emailPattern = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+
+		
+		if(!namePattern.test(firstName) || !namePattern.test(lastName) || !emailPattern.test(email))
+		{
+			return false;
+		}
+		return true;
+	}
